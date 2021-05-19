cd8cf4068c6ef33e62f1e4c82aa1abc6c0b2bb20
kevinmalan <kmalan@outlook.com>
2021-05-19 19:40:56 +0200

    Project Setup

---
+++ Payroll.MVC/Controllers/TaxCalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Payroll.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payroll.MVC.Controllers
+{
+    public class TaxCalculatorController : Controller
+    {
+        private readonly ILogger<TaxCalculatorController> _logger;
+
+        public TaxCalculatorController(ILogger<TaxCalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
