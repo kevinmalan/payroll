79b0788d59f527ef0329119bb6176f1925bcc1f4
kevinmalan <kmalan@outlook.com>
2021-05-20 10:16:06 +0200

    Added FlatRateCalculator Service

---
+++ Payroll.MVC/Services/FlatRateCalculator.cs
@@ -0,0 +1,19 @@
+﻿using Payroll.MVC.Services.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace Payroll.MVC.Services
+{
+    public class FlatRateCalculator : ITaxRateCalculator
+    {
+        public async Task<decimal> CalculateTaxAmountAsync(decimal annualIncome)
+        {
+            if (annualIncome < 0M)
+            {
+                throw new ArgumentException($"The provided annual income '{annualIncome}' should not be below 0.");
+            }
+
+            return annualIncome * (17.5M / 100M);
+        }
+    }
+}
--- Payroll.Tests/TaxRateCalculationTests.cs
+++ Payroll.Tests/TaxRateCalculationTests.cs
@@ -1,12 +1,9 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Shouldly;
-using Payroll.MVC.Services.Contracts;
 using Payroll.MVC.Services;
+using Payroll.MVC.Services.Contracts;
+using Shouldly;
+using System;
+using System.Threading.Tasks;
 
 namespace Payroll.Tests
 {
@@ -14,16 +11,34 @@ namespace Payroll.Tests
     public class TaxRateCalculationTests
     {
         [Test]
-        public async Task CalculateTaxRate_WhenFlatRate_ShouldCalculateCorrectly()
+        public async Task CalculateFlatRateTaxRate_WhenValidRequest_ShouldCalculateCorrectly()
         {
             // Arrange
-            ITaxRateCalculator taxCalculator = new TaxRateCalculator();
+            ITaxRateCalculator taxCalculator = new FlatRateCalculator();
 
             // Act
-            var taxAmount = await taxCalculator.CalculateTaxAmountAsync(200000);
+            var taxAmount1 = await taxCalculator.CalculateTaxAmountAsync(200000);
+            var taxAmount2 = await taxCalculator.CalculateTaxAmountAsync(90540.32M);
+            var taxAmount3 = await taxCalculator.CalculateTaxAmountAsync(0);
 
             // Assert
-            taxAmount.ShouldBe(35000M);
+            taxAmount1.ShouldBe(35000M);
+            taxAmount2.ShouldBe(15844.556M);
+            taxAmount3.ShouldBe(0);
+        }
+
+        [Test]
+        public async Task CalculateFlatRateTaxRate_WhenInvalidRequest_ShouldThrowException()
+        {
+            // Arrange
+            ITaxRateCalculator taxCalculator = new FlatRateCalculator();
+
+            // Act
+            var exception = await Should.ThrowAsync<ArgumentException>(async () => await taxCalculator.CalculateTaxAmountAsync(-200000));
+
+            // Assert
+            exception.GetType().ShouldBe(typeof(ArgumentException));
+            exception.Message.ShouldBe("The provided annual income '-200000' should not be below 0.");
         }
     }
 }
