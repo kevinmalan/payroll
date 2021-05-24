53d87c4d35da09b07dd05179de11315dd21768b7
kevinmalan <kmalan@outlook.com>
2021-05-24 10:12:21 +0200

    Added Vue and Axios

--- Payroll.MVC/wwwroot/js/site.js
+++ Payroll.MVC/wwwroot/js/site.js
@@ -1,1 +1,28 @@
-﻿
+﻿var app = new Vue({
+    el: '#taxCalculator',
+    data: {
+        postalCodes: []
+    }
+});
+
+function getPostalCodes() {
+    axios.get('/api/postalcode')
+        .then((res) => {
+            app.postalCodes = res.data;
+        });
+}
+
+function calculateTax() {
+    axios.post('/api/taxcalculator',
+        {
+            postalCode: "",
+            annualIncome: ""
+        })
+        .then((res) => {
+            // res data binding
+        });
+}
+
+(function onLoad() {
+    getPostalCodes();
+})();
